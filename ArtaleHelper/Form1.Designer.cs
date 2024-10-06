d7cb4ed49c09dc3aa1900b6a141b53f77f8d627e
PanzerWaltz <vosky3279@gmail.com>
2024-10-07 02:40:48 +0900

    Initial commit

---
+++ ArtaleHelper/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ArtaleHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonActive = new System.Windows.Forms.Button();
+            this.labelActive = new System.Windows.Forms.Label();
+            this.buttonKeyInput = new System.Windows.Forms.Button();
+            this.labeKeyBind = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.checkBoxAlarm = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonActive
+            // 
+            this.buttonActive.BackColor = System.Drawing.Color.Transparent;
+            this.buttonActive.BackgroundImage = global::ArtaleHelper.Properties.Resources.imageButton;
+            this.buttonActive.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonActive.FlatAppearance.BorderSize = 0;
+            this.buttonActive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonActive.Location = new System.Drawing.Point(248, 150);
+            this.buttonActive.Name = "buttonActive";
+            this.buttonActive.Size = new System.Drawing.Size(85, 40);
+            this.buttonActive.TabIndex = 0;
+            this.buttonActive.TabStop = false;
+            this.buttonActive.UseVisualStyleBackColor = false;
+            this.buttonActive.Click += new System.EventHandler(this.buttonActive_Click);
+            // 
+            // labelActive
+            // 
+            this.labelActive.AutoSize = true;
+            this.labelActive.BackColor = System.Drawing.Color.Transparent;
+            this.labelActive.Font = new System.Drawing.Font("돋움", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelActive.Location = new System.Drawing.Point(142, 164);
+            this.labelActive.Name = "labelActive";
+            this.labelActive.Size = new System.Drawing.Size(100, 12);
+            this.labelActive.TabIndex = 1;
+            this.labelActive.Text = "상단 고정 : False";
+            // 
+            // buttonKeyInput
+            // 
+            this.buttonKeyInput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonKeyInput.FlatAppearance.BorderSize = 0;
+            this.buttonKeyInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKeyInput.Font = new System.Drawing.Font("돋움", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonKeyInput.Location = new System.Drawing.Point(248, 51);
+            this.buttonKeyInput.Name = "buttonKeyInput";
+            this.buttonKeyInput.Size = new System.Drawing.Size(75, 38);
+            this.buttonKeyInput.TabIndex = 2;
+            this.buttonKeyInput.TabStop = false;
+            this.buttonKeyInput.Text = "None";
+            this.buttonKeyInput.UseVisualStyleBackColor = false;
+            this.buttonKeyInput.Click += new System.EventHandler(this.buttonKeyInput_Click);
+            // 
+            // labeKeyBind
+            // 
+            this.labeKeyBind.AutoSize = true;
+            this.labeKeyBind.BackColor = System.Drawing.Color.Transparent;
+            this.labeKeyBind.Font = new System.Drawing.Font("돋움", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labeKeyBind.Location = new System.Drawing.Point(265, 36);
+            this.labeKeyBind.Name = "labeKeyBind";
+            this.labeKeyBind.Size = new System.Drawing.Size(41, 12);
+            this.labeKeyBind.TabIndex = 3;
+            this.labeKeyBind.Text = "키설정";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.BackColor = System.Drawing.Color.Transparent;
+            this.labelTimer.Font = new System.Drawing.Font("맑은 고딕", 32F, System.Drawing.FontStyle.Bold);
+            this.labelTimer.Location = new System.Drawing.Point(23, 36);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(136, 59);
+            this.labelTimer.TabIndex = 4;
+            this.labelTimer.Text = "00:00";
+            // 
+            // checkBoxAlarm
+            // 
+            this.checkBoxAlarm.AutoSize = true;
+            this.checkBoxAlarm.BackColor = System.Drawing.Color.Transparent;
+            this.checkBoxAlarm.Checked = true;
+            this.checkBoxAlarm.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxAlarm.Font = new System.Drawing.Font("돋움", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.checkBoxAlarm.Location = new System.Drawing.Point(255, 104);
+            this.checkBoxAlarm.Name = "checkBoxAlarm";
+            this.checkBoxAlarm.Size = new System.Drawing.Size(60, 16);
+            this.checkBoxAlarm.TabIndex = 6;
+            this.checkBoxAlarm.Text = "알림음";
+            this.checkBoxAlarm.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ArtaleHelper.Properties.Resources.imageBG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(344, 201);
+            this.Controls.Add(this.checkBoxAlarm);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labeKeyBind);
+            this.Controls.Add(this.buttonKeyInput);
+            this.Controls.Add(this.labelActive);
+            this.Controls.Add(this.buttonActive);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "장판타이머";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonActive;
+        private System.Windows.Forms.Label labelActive;
+        private System.Windows.Forms.Button buttonKeyInput;
+        private System.Windows.Forms.Label labeKeyBind;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.CheckBox checkBoxAlarm;
+    }
+}
+
---
+++ ArtaleHelper/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Windows.Forms.Timer;
+
+namespace ArtaleHelper
+{
+    public partial class Form1 : Form
+    {
+        private bool isActive = false;
+        private bool isFocused = true;
+        private bool keyFlag = false;
+        private bool isAlarmPlayed = false;
+
+        private Keys boundKey = Keys.None;
+        private System.Windows.Forms.Timer timer;
+        private TimeSpan timeLeft;
+        private SoundPlayer alarmSound = null;
+        private string soundFilePath = null;
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitSound();
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            this.MouseClick += new MouseEventHandler(Form1_MouseClick);
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void buttonActive_Click(object sender, EventArgs e)
+        {
+            isActive = !isActive;
+
+            labelActive.Text = $"상단 고정 : {isActive}";
+            TopMost = isActive;
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            isFocused = !isFocused;
+
+            if (isFocused)
+            {
+                this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.None;
+            }
+        }
+
+        private void buttonKeyInput_Click(object sender, EventArgs e)
+        {
+            keyFlag = true;
+            buttonKeyInput.Text = " ";
+        }
+
+        private async  void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (keyFlag)
+            {
+                if (e.KeyCode == Keys.Escape)
+                {
+                    keyFlag = false;
+                    buttonKeyInput.Text = "None";
+                    boundKey = Keys.None;
+
+                    timeLeft = TimeSpan.FromMinutes(1) + TimeSpan.FromSeconds(30);
+                    timer.Stop();
+                    UpdateTimerLabel();
+                }
+                else
+                {
+                    boundKey = e.KeyCode;
+                    buttonKeyInput.Text = boundKey.ToString(); 
+                    await Task.Delay(100);
+                    keyFlag = false;
+                }
+            }
+        }
+
+        private void StartTimer()
+        {
+            timeLeft = TimeSpan.FromMinutes(1) + TimeSpan.FromSeconds(30);
+            timer.Start();
+            UpdateTimerLabel();
+            labelTimer.ForeColor = Color.Black;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft.TotalSeconds > 0)
+            {
+                timeLeft = timeLeft.Subtract(TimeSpan.FromSeconds(1));
+                UpdateTimerLabel();
+
+                if (timeLeft.TotalSeconds <= 10)
+                {
+                    labelTimer.ForeColor = Color.Red;
+                    if(isAlarmPlayed == false)
+                    {
+                        PlayAlarm();
+                        isAlarmPlayed = true;
+                    }
+                }
+            }
+            else
+            {
+                timer.Stop();
+            }
+        }
+
+        private void UpdateTimerLabel()
+        {
+            labelTimer.Text = string.Format("{0:D2}:{1:D2}", timeLeft.Minutes, timeLeft.Seconds);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {           
+            Thread TH = new Thread(BGKeyDetect);
+            TH.SetApartmentState(ApartmentState.STA);
+            TH.IsBackground = true;
+            TH.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isRunning = false;
+        }
+
+        private bool isRunning = true;
+        private void BGKeyDetect()
+        {
+            while (isRunning)
+            {
+                Thread.Sleep(100);
+
+                // 키 바인딩 중일 때는 타이머를 시작하지 않도록 함
+                if (keyFlag)
+                {
+                    continue; // keyFlag가 true면 키 감지 스킵
+                }
+
+                if (boundKey != Keys.None && GetAsyncKeyState(boundKey) < 0)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        timeLeft = TimeSpan.FromMinutes(1) + TimeSpan.FromSeconds(30);
+                        labelTimer.ForeColor = Color.Black;
+                        isAlarmPlayed = false;
+                        if (!timer.Enabled)
+                        {
+                            timer.Start();
+                        }
+                        UpdateTimerLabel();
+                    }));
+                }
+            }
+        }
+
+        #region Sound 
+        private void PlayAlarm()
+        {
+            if (checkBoxAlarm.Checked && alarmSound != null)
+            {
+                alarmSound.Play(); 
+            }
+        }
+
+        private void InitSound()
+        {
+            soundFilePath = "alarm.wav";
+            if (System.IO.File.Exists(soundFilePath))
+            {   
+                alarmSound = new SoundPlayer(soundFilePath);
+                MessageBox.Show("알림음이 설정되었습니다.", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                alarmSound = null;
+                MessageBox.Show("알림음을 찾을 수 없습니다.\n실행파일과 같은 위치에 alarm.wav를 놓고 다시 실행해 주세요.", "경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
+    }
+}
